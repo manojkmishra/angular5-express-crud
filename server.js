22a4c7dabd0ee0bec3b38f3342839c8ac7dbc0c6
manojkmishra <manojkm1980@gmail.com>
2018-04-17 14:34:52 +1000

    mlab setup , crud api done

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var api = require('./server/routes/api');
+
 
 var app= express();
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -9,9 +12,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api', api);
-app.get('*',(req,res)=> {
-  res.sendFile(path.join(__dirname,'dist/index.html'));
-});
-app.listen(3000, function(){
-  console.log("listen to port 3000");
-});
+app.get('*',(req,res)=> {  res.sendFile(path.join(__dirname,'dist/index.html'));    }); //define entry as index.html in dist folder
+app.listen(3000, function(){  console.log("Server running on port 3000");  });
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,67 @@
+
 var express= require('express');
 var router = express.Router();
+var mongojs = require('mongojs');  
+var db= mongojs('mongodb://angular:angular@ds113936.mlab.com:13936/angularcrud',['angularcrud'])
+//var db=process.env.DB
+//var db= mongojs(process.env.DB,['angularcrud'])
 
-router.get('/', function(req, res, next){
-req.setEncoding('api page is working')
+//-------get all data of the collection
+router.get('/', function(req, res, next)
+{       //res.send('/server/routes/api.js- api page is working');
+        //console.log('req.roue.path=',req.route.path);  //visible on dos window
+    db.angularcrud.find(function(err,angularcrud)
+        {   if(err){   res.send(err); }
+            else   {  res.json(angularcrud);  }
+        });
 });
+//---------------get data baed on id field
+router.get('/:id', function(req, res, next)
+                {   db.angularcrud.findOne(
+                                        {   _id:mongojs.ObjectId(req.params.id)    },
+                                        function(err, angularcrud)
+                                        {   if(err){ res.send(err); }
+                                            else{  res.json(angularcrud);  }
+                                        }
+                                    );
+                }
+          );
+//-------------save data in collection
+router.post('/', function(req, res, next)
+{   var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + ''))
+    {   res.status(404);
+        res.json({  "error":"invalid Data"  });
+    }
+  else{   db.angularcrud.save(todo, function(err, result)
+              { if(err){ res.send(err); }else{  res.json(result);  }
+              });
+       }
+});
+
+//---update data in collection
+router.put('/:id', function(req, res, next)
+{   var todo = req.body;
+    var updObj = {};
+    if(todo.isCompleted){   updObj.isCompleted = todo.isCompleted    }
+    if(todo.text){      updObj.text = todo.text;    }
+    if(!updObj){     res.status(404);
+                     res.json({        "error":"invalid Data"      });
+              }
+    else{  db.angularcrud.update({  _id: mongojs.ObjectId(req.params.id) }, 
+                             updObj,{},function(err, result)
+                               {  if(err){   res.send(err); }else{  res.json(result);  }
+                               });
+        }
+});
+  // Delete 
+router.delete('/:id', function(req, res, next)
+{   db.angularcrud.remove({   _id: mongojs.ObjectId(req.params.id)  }, '', 
+                     function(err, result)
+                        {  if(err){  res.send(err); } else { res.json(result); }
+                        }
+                    );
+});
+  
 
 module.exports = router;
